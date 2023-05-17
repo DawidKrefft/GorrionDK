45da8b0bbc7f9e072b2308140b5fe374449bab9b
davvidkrefft <davvidkrefft@gmail.com>
2023-05-17 22:34:09 +0200

    create pagination endpoint, api fetch

--- pages/api/people.ts
+++ pages/api/people.ts
@@ -1,11 +1,5 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-
-type User = {
-  name: string;
-  email: string;
-  title: string;
-  role: string;
-};
+import { faker } from "@faker-js/faker";
 
 /**
  * TODO: Prepare an endpoint to return a list of users
@@ -14,9 +8,42 @@ type User = {
  * The endpoint should accept a query parameter "page" to return the corresponding page
  */
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<User[]>
-) {
-  res.status(200).json([]);
+// setting up fake users data
+export type User = {
+  name: string;
+  email: string;
+  title: string;
+  role: string;
+};
+export function createRandomUser(): User {
+  return {
+    name: faker.person.fullName(),
+    email: faker.internet.email(),
+    title: faker.person.jobTitle(),
+    role: faker.person.jobType(),
+  };
+}
+export const USERS: User[] = faker.helpers.multiple(createRandomUser, {
+  count: 118,
+});
+
+const sortedUsers = USERS.sort((a, b) => a.name.localeCompare(b.name));
+
+// creating api endpoint for pagination
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  const { page = 1, limit = 10 } = req.query;
+
+  const startIndex = (Number(page) - 1) * Number(limit);
+  const endIndex = startIndex + Number(limit);
+
+  const paginatedUsers = sortedUsers.slice(startIndex, endIndex);
+  const totalPages = Math.ceil(sortedUsers.length / Number(limit));
+
+  if (req.method === "GET") {
+    const totalRecords = sortedUsers.length;
+
+    res.status(200).json([paginatedUsers, totalPages, totalRecords]);
+  } else {
+    res.status(405).json({ message: "Method Not Allowed" });
+  }
 }
---
+++ services/apiFetch.ts
@@ -0,0 +1,25 @@
+import { User } from "../pages/api/people";
+import { ITEMS_PER_PAGE } from "@/utils/constants";
+
+type FetchPeopleResponse = {
+  people: User[];
+};
+
+export const fetchPeople = async (
+  page: number
+): Promise<FetchPeopleResponse> => {
+  try {
+    const response = await fetch(
+      `http://localhost:3000/api/people?page=${page}&limit=${ITEMS_PER_PAGE}`
+    );
+    const totalData = response.headers.get("X-Total-Count");
+    const data = await response.json();
+
+    return {
+      people: data,
+    };
+  } catch (error) {
+    console.error(error);
+    throw new Error("Failed to fetch people");
+  }
+};
