089e06c89054f4463fe90a784f5bf00f00f6ad7f
davvidkrefft <davvidkrefft@gmail.com>
2023-05-17 22:54:11 +0200

    setup react-query, fetch data

--- pages/task.tsx
+++ pages/task.tsx
@@ -1,15 +1,26 @@
+import { useQuery } from "@tanstack/react-query";
+import { useState } from "react";
+import { fetchPeople } from "../services/apiFetch";
+import { User } from "./api/people";
+
 export default function Task() {
   /**  TODO: Create an endpoint that returns a list of people, and use that here.
    * Use tanstack/react-query to fetch the data
    */
-  const people = [
-    {
-      name: "Jane Cooper",
-      email: "jane@cooper.com",
-      title: "Regional Paradigm Technician",
-      role: "Admin",
-    },
-  ];
+
+  // page state
+  const [currentPage, setCurrentPage] = useState<number>(1);
+
+  // data fetch using tanstack/react-query
+  const { isLoading, isError, error, data, isFetching, isPreviousData } =
+    useQuery(["people", currentPage], () => fetchPeople(currentPage), {
+      keepPreviousData: true,
+    });
+  // variables for fetched data
+  const totalPeople = data?.people[0];
+  const totalPages = Number(data?.people[1]);
+  const totalRecords = Number(data?.people[2]);
+
   return (
     <div className="mx-auto max-w-7xl">
       <div className="mt-8 flow-root">
@@ -48,22 +59,28 @@ export default function Task() {
                 </tr>
               </thead>
               <tbody className="divide-y divide-gray-800">
-                {people.map((person) => (
-                  <tr key={person.email}>
-                    <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-white sm:pl-0">
-                      {person.name}
-                    </td>
-                    <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-300">
-                      {person.title}
-                    </td>
-                    <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-300">
-                      {person.email}
-                    </td>
-                    <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-300">
-                      {person.role}
-                    </td>
+                {Array.isArray(totalPeople) ? (
+                  totalPeople.map((person: User) => (
+                    <tr key={person.email}>
+                      <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-white sm:pl-0">
+                        {person.name}
+                      </td>
+                      <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-300">
+                        {person.title}
+                      </td>
+                      <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-300">
+                        {person.email}
+                      </td>
+                      <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-300">
+                        {person.role}
+                      </td>
+                    </tr>
+                  ))
+                ) : (
+                  <tr>
+                    <td colSpan={4}>No people found.</td>
                   </tr>
-                ))}
+                )}
               </tbody>
             </table>
             {/* TODO: Pagination */}
